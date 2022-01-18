540901e0d406db1a136858acf9db670198cabbbf
Aliyan-Ansari <81861676+Aliyan-Ansari@users.noreply.github.com>
2022-01-17 16:30:44 -0800

    survey question

---
+++ firebase.js
@@ -0,0 +1,16 @@
+  // <!-- --------- Firebase config ---------- -->
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHrE2vN7i4H5aC4MSOIUGIlQmz3RvOa50",
+  authDomain: "covid19survey-f4d65.firebaseapp.com",
+  projectId: "covid19survey-f4d65",
+  storageBucket: "covid19survey-f4d65.appspot.com",
+  messagingSenderId: "847426064185",
+  appId: "1:847426064185:web:82004f9fb489919059a2e8"
+};
+
+// firebase.initializeApp(firebaseConfig);
+// v9 compat packages are API compatible with v8 code
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 // JS File for login
+type = "module";
+    import {getAuth} from "firebase/auth";
+document.getElementById('loginForm').addEventListener("submit",(e) =>{
+	e.preventDefault();
+	login();
+})
+
+firebase.auth().onAuthStateChanged((user) =>{
+	if(user){
+		location.replace("panel.html");
+	}
+})
+
+function login(){
+	const email = document.getElementById("email").value;
+	const password = document.getElementById("password").value;
+	const auth = getAuth();
+	signInWithEmailAndPassword(auth, email, password)
+  	.then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
+// document.getElementById("loginForm").addEventListener("submit",(event)=>{
+//     event.preventDefault()
+// })
+
+// firebase.auth().onAuthStateChanged((user)=>{
+//     if(user){
+//         location.replace("panel.html")
+//     }
+// })
+
+// function login(){
+//     const email = document.getElementById("email").value
+//     const password = document.getElementById("password").value
+//     firebase.auth().signInWithEmailAndPassword(email, password)
+//     .catch((error)=>{
+//         document.getElementById("error").innerHTML = error.message
+//     })
+// }
